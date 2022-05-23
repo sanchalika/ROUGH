1d02dab55c3c51c3219538ea83e73e565a494640
Sanchalika <sanchalika2019@gmail.com>
2022-05-23 22:10:47 +0530

    Add files via upload

---
+++ src/screens/ProductScreen.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import {Row, Col, Image, ListGroup, Card, Button, ListGroupItem} from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
+
+const ProductScreen = ({match}) => {
+
+    const product=products.find(p => p._id === match.params.id)
+    
+    return <>
+        <Link className='btn btn-light my-3' to='/'>
+            Go Back
+        </Link>
+        <Row>
+            <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid/>
+            </Col>
+            <Col md={3}>
+                <ListGroup variant='flush'>
+                    <ListGroup.Item>
+                        <h3>{product.name}</h3>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Rating value={product.rating} text={`${product.numReviews}`} />
+                    </ListGroup.Item>
+                    <ListGroupItem>Price: ${product.price}</ListGroupItem>
+                    <ListGroupItem>Price: ${product.description}</ListGroupItem>
+                </ListGroup>
+            </Col>
+        </Row>
+        </>
+}
+
+export default ProductScreen
